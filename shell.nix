e4eee84ea659ac674bf78685beba21c4f94b3470
Max Tyler <maxastyler@gmail.com>
2021-02-25 09:17:24 +0000

    Update the shell

--- shell.nix
+++ shell.nix
@@ -3,7 +3,7 @@
 pkgs.mkShell {
   buildInputs = [
     pkgs.hello
-    (pkgs.python3.withPackages (ps: with ps; [numpy]))
+    (pkgs.python3.withPackages (ps: with ps; [numpy python-language-server pyls-mypy]))
     # keep this line if you use bash
     pkgs.nixfmt
     pkgs.bashInteractive
