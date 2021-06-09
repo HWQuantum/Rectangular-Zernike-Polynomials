25e9226596aace6c4ad43be09f97c69ecd11ad36
Max Tyler <maxastyler@gmail.com>
2021-06-09 12:32:03 +0100

    Add in typing

--- rect_zern/rect_zern.py
+++ rect_zern/rect_zern.py
@@ -1,8 +1,9 @@
 """Main module."""
 import numpy as np
+from typing import List, Tuple, Generator, Callable, Iterable
 
 
-def radial_coeff(m, n, l):
+def radial_coeff(m: int, n: int, l: int) -> int:
     '''Radial coefficient in the sum for the radial part of the Zernike polynomial
     '''
     if (n - m) % 2 == 0:
@@ -13,7 +14,7 @@ def radial_coeff(m, n, l):
         return 0
 
 
-def radial(m, n):
+def radial(m: int, n: int) -> List[Tuple[int, int]]:
     '''Radial part of the Zernike polynomial
     '''
     return [(radial_coeff(m, n, l), n - 2 * l)
@@ -21,7 +22,7 @@ def radial(m, n):
                            int(round((n - m) / 2)) + 1)]
 
 
-def coefficients(n):
+def coefficients(n: int) -> Generator[Tuple[int, int], None, None]:
     '''Generate the first n non-zero Zernike coefficient indices
     ie: (0, 0), (-1, 1), (1, 1), (-2, 2), (0, 2), (2, 2), ...
     '''
@@ -37,7 +38,8 @@ def coefficients(n):
             i += 2
 
 
-def zernike_cartesian(m, n):
+def zernike_cartesian(
+        m: int, n: int) -> Callable[[np.ndarray, np.ndarray], np.ndarray]:
     '''Returns a function to get the Zernike polynomial in cartesian coordinates
     '''
     abs_m = np.abs(m)
@@ -66,18 +68,21 @@ def zernike_cartesian(m, n):
         return poly
 
 
-def rect_coords(w, h):
+def rect_coords(w: int, h: int) -> Tuple[np.ndarray, np.ndarray]:
     """Get the rectangle coordinates with area pi for the given width and height
     Returns a numpy array with the correct coordinates
     """
-    ratio = w/h
-    norm_h = np.sqrt(np.pi/ratio)
+    ratio = w / h
+    norm_h = np.sqrt(np.pi / ratio)
     norm_w = norm_h * ratio
-    x, y = np.mgrid[-norm_w/2:norm_w/2:w*1j, -norm_h/2:norm_h/2:h*1j]
+    x, y = np.mgrid[-norm_w / 2:norm_w / 2:w * 1j,
+                    -norm_h / 2:norm_h / 2:h * 1j]
     return x, y
 
 
-def gram_schmidt(modes, x, y):
+def gram_schmidt(modes: Iterable[Callable[[np.ndarray, np.ndarray],
+                                          np.ndarray]], x: np.ndarray,
+                 y: np.ndarray) -> List[np.ndarray]:
     """Perform a gram-schmidt orthonormalisation over the given modes. 
     The modes should be functions which operate like modes[i](x, y)
     Returns a list of numpy array modes.
@@ -88,10 +93,12 @@ def gram_schmidt(modes, x, y):
         m = mode_rep[i] - sum(np.sum(m * mode_rep[i]) for m in new_modes[:i])
         new_modes.append(m)
 
-    return [m/(np.sqrt(np.pi*np.sum(m*m))) for m in new_modes]
+    return [m / (np.sqrt(np.pi * np.sum(m * m))) for m in new_modes]
 
 
-def rectangular_zernike_modes(n_modes, w, h):
+def rectangular_zernike_modes(
+        n_modes: int, w: int,
+        h: int) -> Tuple[List[np.ndarray], np.ndarray, np.ndarray]:
     """Given a number of modes to generate, a width and a height return a list containing 
     the modes, and the coordinates over which they're represented.
     """
